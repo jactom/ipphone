0b72ca3cb89e269fe00777f2074f0f1394dbffc2
Tomin Sebastian <tominsebastian1@gmail.com>
2017-04-28 22:37:39 +0530

    original sourses from test branch added

---
+++ chat_client.c
@@ -0,0 +1,175 @@
+/* client programme for the chat application. client connect to server and send messages*/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <pulse/gccmacro.h>
+
+#include "g711.c"
+#define MAX_SIZE 8192//character buffer size
+
+int sock; //socket file descriptor.
+pa_simple *s = NULL;
+timer_t timerid;
+
+void sampler (int signal);
+static ssize_t loop_write(int, const void*, size_t);
+/* A simple routine calling UNIX write() in a loop */
+
+/* client signal handler */
+void sig_handler(int signumber)
+{
+	char ans[2];
+	if(signumber == SIGINT){
+		printf("receved CTRL-C\n");
+		printf("Terminate y/n :");
+		scanf("%s", ans);
+
+		if(strcmp(ans, "y") == 0){
+			printf("Exiting....\n");
+//			if (s)
+//				pa_simple_free(s);
+			close(sock);
+            timer_delete(timerid);
+			exit(0);
+		}else{
+			printf("Continuing..\n");
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 3){ //usage is .cc <ip> <port>
+		fprintf(stderr,"Usage:cc ip port\n");
+		exit(1);
+	}
+
+	int c = 0;
+	struct sockaddr_in server;
+
+   // pthread_attr_t attr;
+   // pthread_attr_init(&attr);
+
+   // struct sched_param parm;
+   // parm.sched_priority = 255;
+   // pthread_attr_setschedparam(&attr, &parm);
+
+	static const pa_sample_spec ss = {
+		.format = PA_SAMPLE_S16LE,
+		.rate = 44100,
+		.channels = 2
+	};
+
+    struct itimerspec per;
+    per.it_value.tv_sec = 0;
+    per.it_value.tv_nsec =100000;
+    per.it_interval.tv_sec = 0;
+    per.it_interval.tv_nsec = 100000;
+	int ret = 1;
+	int error;
+
+
+    /* create timer handler */
+    //struct sigevent sig;
+    //sig.sigev_notify = SIGEV_THREAD;
+    //sig.sigev_notify_function = sampler;
+    //sig.sigev_value.sival_int = 10;
+    //sig. sigev_notify_attributes = &attr;
+
+    if (signal(SIGALRM, sampler) == SIG_ERR) {
+      perror("Signal");
+    }
+
+    if ((timer_create(CLOCK_REALTIME, NULL, &timerid)) == -1) {
+      perror("timer: failed");
+      exit(1);
+    }
+    
+    printf("timer created\n");
+
+	if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
+		fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+		exit(1);
+	}
+
+	/* create socket*/
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("server: socket");
+		exit(1);
+	}
+
+	printf("Socket created\n");
+
+	/* configure socket */
+	server.sin_addr.s_addr = inet_addr(argv[1]);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(atoi(argv[2]));
+
+	/* connect */
+	if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0){
+		perror("Connect failed.");
+		exit(1);
+	}
+
+	printf("Connected\n");
+
+	/* catch signal*/
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+		printf("\ncan't catch SIGINT\n");
+
+    /* start timer */
+    if ((timer_settime(timerid, 0, &per,  NULL)) == -1) {
+      perror("timer_settime");
+      exit(1);
+    }
+
+	/*client alwas run! */
+	while (1) {
+
+
+	}
+
+
+}
+
+
+void sampler (int signal)
+{
+
+  uint16_t buf1[MAX_SIZE];
+  uint8_t buf[MAX_SIZE];
+  int len, i;
+  int error;
+  if (signal == SIGALRM) {
+    /* catch audio... */
+    if (pa_simple_read(s, buf1, sizeof(buf1), &error) < 0) {
+      fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+    }
+
+    len = sizeof(buf1) / sizeof(buf1[0]);
+
+    for(i = 0; i < len; i++) {
+      buf[i] = Snack_Lin2Mulaw(buf1[i]);
+    }  
+
+   // if (loop_write(sock, buf, sizeof(buf)) != sizeof(buf)) {
+   //   fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
+   // }
+    if (send(sock, buf, sizeof(buf), 0) == -1){
+      perror("send");
+    }
+  }
+}
---
+++ chat_server.c
@@ -0,0 +1,155 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <pulse/gccmacro.h>
+#include "g711.c"
+
+#define MAX_SIZE 8192 
+#define BACKLOG 3
+
+int s_sock, c_sock; //file descriptors for sockets
+    pa_simple *s = NULL;
+
+/* signal handler */
+void sig_handler(int signumber)
+{
+    if (signumber == SIGINT) {
+        printf("\nreceived CTRL-C\n Terminating....\n");
+        close(s_sock);
+        close(c_sock);
+
+        if (s)
+            pa_simple_free(s);
+
+        exit(0);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2){// usage : ./cs <port number>
+        fprintf(stderr,"usage : cs port\n");
+        exit(1);
+    }
+
+    int i, j, len,  numbytes;
+    struct sockaddr_in server, client;
+    uint8_t message_buf[MAX_SIZE];
+    uint16_t m_buf[MAX_SIZE];
+
+    static const pa_sample_spec ss = {
+        .format = PA_SAMPLE_S16LE,
+        .rate = 44100,
+        .channels = 2
+    };
+
+    int ret = 1;
+    int error;
+
+    /* create socket */
+    if((s_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("server: socket");
+        exit(1);
+    }
+
+    printf("Socket created\n");
+
+    /* clear memory to avoid undefined behaviour */
+    memset(&server, 0, sizeof(server));
+
+    /* configure socket */
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(atoi(argv[1]));
+
+    /* binding socket */
+    if(bind(s_sock, (struct sockaddr *)&server, sizeof(server)) < 0){
+        perror("bind");
+        exit(1);
+    }
+
+    printf("bind done\n");
+
+    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error))) {
+        fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        exit(1);
+    }
+
+    /* listening */
+    listen(s_sock, BACKLOG);
+
+    /*catch signal */
+    if(signal(SIGINT, sig_handler) == SIG_ERR)
+        printf("\n can't catch sigint\n");
+
+    /* server waits for client */
+    while(1){
+
+        printf("Waiting for incoming connections...\n");
+        i = sizeof(struct sockaddr_in);
+
+        /* accept client connection */
+        if((c_sock = accept(s_sock, (struct sockaddr *)&client, (socklen_t*)&i)) < 0){
+            perror("accept failed");
+            exit(1);
+        }
+
+        printf("Connection accepted\n");
+
+        /* receive client audio*/
+        while((numbytes = recv(c_sock, message_buf, sizeof(message_buf), 0)) > 0){
+#if 0
+            pa_usec_t latency;
+
+            if ((latency = pa_simple_get_latency(s, &error)) == (pa_usec_t) -1) {
+                fprintf(stderr, __FILE__": pa_simple_get_latency() failed: %s\n", pa_strerror(error));
+                goto finish;
+            }
+
+            fprintf(stderr, "%0.0f usec    \r", (float)latency);
+#endif
+            /* ... and play it */
+            len = sizeof(message_buf) / sizeof(message_buf[0]);
+            for(j = 0; j < len ; j++) {
+              m_buf[j] = Snack_Mulaw2Lin(message_buf[j]);
+            }
+
+            if (pa_simple_write(s, m_buf, (size_t) sizeof(m_buf), &error) < 0) {
+                fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
+            }
+
+
+        }
+
+        if (numbytes == 0) {// if client disconnected wait for another.
+            if (pa_simple_drain(s, &error) < 0) {
+                fprintf(stderr, __FILE__": pa_simple_drain() failed: %s\n", pa_strerror(error));
+            }
+
+            if (pa_simple_flush(s, &error) < 0) {
+                fprintf(stderr, __FILE__": pa_simple_flush() failed: %s\n", pa_strerror(error));
+            }
+
+            printf("Client disconnected\n");
+            fflush(stdout);
+            continue;
+        }
+
+        if(numbytes == -1)
+            perror("recv failed");
+    }
+
+
+    return 0;
+}
